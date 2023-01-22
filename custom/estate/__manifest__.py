9ecd6a7785e71bab38c8bf2f21e9a0f33240ab1b
Patrik Brosz <patrik.brosz1@gmail.com>
2023-01-22 17:31:40 +0100

    Defining estate menus in manifest

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -3,6 +3,7 @@
     'depends': ['base'],
 
     'data': ['views/estate_property_views.xml',
+             'views/estate_menus.xml',
              'security/ir.model.access.csv'],
 
     'application': 'True'
