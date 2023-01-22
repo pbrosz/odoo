c269eea515d59415a16f3cee1213ed296259172b
Patrik Brosz <patrik.brosz1@gmail.com>
2023-01-22 13:07:07 +0100

    Creating action for estate.property model

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -2,7 +2,8 @@
     'name': 'Real Estate',
     'depends': ['base'],
 
-    'data': ['security/ir.model.access.csv'],
+    'data': ['views/estate_property_views.xml',
+             'security/ir.model.access.csv'],
 
     'application': 'True'
 }
