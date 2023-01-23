fa7deaed93104fd7428604ae268c21f4e7322b42
Patrik Brosz <patrik.brosz1@gmail.com>
2023-01-23 21:01:06 +0100

    Adding new fields to Estate Property Offer and computation of them

--- custom/estate/models/estate_property_offer.py
+++ custom/estate/models/estate_property_offer.py
@@ -1,4 +1,8 @@
-from odoo import fields, models
+from datetime import datetime, timedelta
+
+from dateutil.utils import today
+
+from odoo import fields, models, api
 
 
 class EstatePropertyOffer(models.Model):
@@ -9,3 +13,24 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection([("accepted", "Accepted"), ("refused", "Refused")], copy=False)
     partner_id = fields.Many2one('res.partner', string='Partner', index=True, required=True)
     property_id = fields.Many2one('estate.property', string='Property', index=True, required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_deadline", inverse="_inverse_deadline")
+
+    @api.depends("validity")
+    def _compute_deadline(self):
+        for record in self:
+            if record.create_date is None:
+                create_date = fields.Date().today()
+            else:
+                create_date = fields.Date().to_date(record.create_date)
+
+            record.date_deadline = create_date + timedelta(days=record.validity)
+
+    def _inverse_deadline(self):
+        for record in self:
+            if record.create_date is None:
+                create_date = fields.Date().today()
+            else:
+                create_date = fields.Date().to_date(record.create_date)
+
+            record.validity = (record.date_deadline - create_date).days
