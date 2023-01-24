f6310f59b811cdb6474b757ca9cd8c8e05d6635b
Patrik Brosz <patrik.brosz1@gmail.com>
2023-01-24 11:35:56 +0100

    Adding sequence ordering in Property Type

--- custom/estate/models/estate_property_type.py
+++ custom/estate/models/estate_property_type.py
@@ -4,9 +4,10 @@ from odoo import fields, models
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Estate Property Type"
-    _order = "name"
+    _order = "sequence, name"
 
     name = fields.Char(required=True)
+    sequence = fields.Integer('Sequence', default=1)
     property_ids = fields.One2many('estate.property', 'property_type_id', string="Properties")
 
     _sql_constraints = [
