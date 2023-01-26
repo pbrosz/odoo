74e890466e75a3bdea49bc728932b234b318d4be
Patrik Brosz <patrik.brosz1@gmail.com>
2023-01-26 03:15:06 +0100

    Changing order of files in manifest

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -6,9 +6,9 @@
     'category': 'Real Estate/Brokerage',
 
     'data': ['views/estate_property_views.xml',
+             'views/estate_property_offer_views.xml',
              'views/estate_property_type_views.xml',
              'views/estate_property_tag_views.xml',
-             'views/estate_property_offer_views.xml',
              'views/res_users_view.xml',
              'views/estate_menus.xml',
              'security/security.xml',
